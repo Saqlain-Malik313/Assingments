1735831cea2cca026d946e369a3fc54fad004837
Saqlain Malik <115017610+Saqlain-Malik313@users.noreply.github.com>
2025-01-22 11:23:44 +0530

    Foot.js

--- src/component/Foot.js
+++ src/component/Foot.js
@@ -9,67 +9,67 @@ const Foot = () => {
                 <Col className=''>
                     <h3 style={{marginLeft:'20px'}}>Product</h3>
                     <h6>
-                        <li><a href='/'>Overview</a></li>
-                        <li><a href='#Feature'>Feature</a></li>
-                        <li><a href='/'>Solution</a></li>
-                        <li><a href='/'>Tutorial</a></li>
-                        <li><a href='/'>Pricing</a></li>
-                        <li><a href='/'>Releases</a></li>
+                        <li><a href='/Overview'>Overview</a></li>
+                        <li><a href='/feature'>Feature</a></li>
+                        <li><a href='/Solution'>Solution</a></li>
+                        <li><a href='/Tutorial'>Tutorial</a></li>
+                        <li><a href='/Pricing'>Pricing</a></li>
+                        <li><a href='/Releases'>Releases</a></li>
                     </h6>
                 </Col>
                 <Col className=''>
                     <h3 style={{marginLeft:'20px'}}>Company</h3>
                     <h6>
-                        <li><a href='/'>About us</a></li>
-                        <li><a href='/'>Careers</a></li>
-                        <li><a href='/'>Press</a></li>
-                        <li><a href='/'>News</a></li>
-                        <li><a href='/'>Media kit</a></li>
-                        <li><a href='/'>Contact</a></li>
+                        <li><a href='/About'>About us</a></li>
+                        <li><a href='/Careers'>Careers</a></li>
+                        <li><a href='/Press'>Press</a></li>
+                        <li><a href='/News'>News</a></li>
+                        <li><a href='/Media'>Media kit</a></li>
+                        <li><a href='/Contact'>Contact</a></li>
                     </h6>
                 </Col>
                 <Col className=''>
                     <h3 style={{marginLeft:'20px'}}>Resources</h3>
                     <h6>
-                        <li><a href='/'>Blog</a></li>
-                        <li><a href='/'>Newsletter</a></li>
-                        <li><a href='/'>Event</a></li>
-                        <li><a href='/'>Help center</a></li>
-                        <li><a href='/'>Tutorials</a></li>
-                        <li><a href='/'>Support</a></li>
+                        <li><a href='/Blog'>Blog</a></li>
+                        <li><a href='/Newsletter'>Newsletter</a></li>
+                        <li><a href='/Event'>Event</a></li>
+                        <li><a href='/Help'>Help center</a></li>
+                        <li><a href='/Tutorials'>Tutorials</a></li>
+                        <li><a href='/Support'>Support</a></li>
                     </h6>
                 </Col>
                 <Col className=''>
                     <h3 style={{marginLeft:'20px'}}>Use cases</h3>
                     <h6>
-                        <li><a href='/'>Startup</a></li>
-                        <li><a href='/'>Enterprise</a></li>
-                        <li><a href='/'>Goverment</a></li>
-                        <li><a href='/'>Saas center</a></li>
-                        <li><a href='/'>MarketPlaces</a></li>
-                        <li><a href='/'>Ecommerce</a></li>
+                        <li><a href='/Startup'>Startup</a></li>
+                        <li><a href='/Enterprise'>Enterprise</a></li>
+                        <li><a href='/Goverment'>Goverment</a></li>
+                        <li><a href='/Saas'>Saas center</a></li>
+                        <li><a href='/MarketPlaces'>MarketPlaces</a></li>
+                        <li><a href='/Ecommerce'>Ecommerce</a></li>
                     </h6>
                 </Col>
                 <Col className=''>
                     <h3 style={{marginLeft:'20px'}}>Social</h3>
                     <h6>
-                        <li><a href='/'>Twitter</a></li>
-                        <li><a href='/'>Linkedin</a></li>
-                        <li><a href='/'>Facebook</a></li>
-                        <li><a href='/'>GitHub </a></li>
-                        <li><a href='/'>AngelList</a></li>
-                        <li><a href='/'>Dribble</a></li>
+                        <li><a href='/Twitter'>Twitter</a></li>
+                        <li><a href='/Linkedin'>Linkedin</a></li>
+                        <li><a href='/Facebook'>Facebook</a></li>
+                        <li><a href='/GitHub'>GitHub </a></li>
+                        <li><a href='/AngelList'>AngelList</a></li>
+                        <li><a href='/Dribble'>Dribble</a></li>
                     </h6>
                 </Col>
                 <Col className=''>
                     <h3 style={{marginLeft:'20px'}}>Legal</h3>
                     <h6>
-                        <li><a href='/'>Terms</a></li>
-                        <li><a href='/'>Privacy</a></li>
-                        <li><a href='/'>Cookies</a></li>
-                        <li><a href='/'>Licenses </a></li>
-                        <li><a href='/'>Settings</a></li>
-                        <li><a href='/'>Contact</a></li>
+                        <li><a href='/terms'>Terms</a></li>
+                        <li><a href='/Privacy'>Privacy</a></li>
+                        <li><a href='/Cookies'>Cookies</a></li>
+                        <li><a href='/Licenses'>Licenses </a></li>
+                        <li><a href='/Settings'>Settings</a></li>
+                        <li><a href='/Contact'>Contact</a></li>
                     </h6>
                 </Col>
             </Row>
