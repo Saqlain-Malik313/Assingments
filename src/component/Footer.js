b45cdd7b980fbbf0b0ced48084303741c6f1c688
Saqlain Malik <115017610+Saqlain-Malik313@users.noreply.github.com>
2025-01-22 15:45:58 +0530

    Footer.js

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -5,16 +5,16 @@ import Foot from './Foot';
 const Footer = () => {
     return (
         <>
-            <div className='container' style={{ paddingTop: "10px" }}>
-                <div className='container bg-body-tertiary' style={{ backgroundcolor: "grey", width: "100%", height: '150%', borderRadius: "10px" }}>
+            <div className='container-fluid' style={{ paddingTop: "10px" }}>
+                <div className='container-fluid00 bg-body-tertiary' style={{ backgroundcolor: "grey", width: "100%", height: '150%', borderRadius: "10px" }}>
                     <div className='center'>
                         <h1>Start your free trail</h1>
                     </div>
                     <p className='center'>Powerful, self-serve product and growth analytics to help you convert, engage,
                         and retain more users. Trusted by over 4,000 startups.</p>
-                    <div className='center container' style={{paddingBottom:"30px"}}>
-                        <div className="wrap container ">
-                            <Button variant="primary" id='btn' size="lg" style={{ borderRadius: "5px" }}>
+                    <div className='center container-fluid' style={{paddingBottom:"30px"}}>
+                        <div className="wrap container-fluid ">
+                            <Button variant="light" id='btn' size="lg" style={{ borderRadius: "5px",border:" .2 px black solid" }}>
                                 Learn more
                             </Button>
                             <Button variant="danger" id='btn' size="lg" style={{ borderRadius: "5px",  }}>
