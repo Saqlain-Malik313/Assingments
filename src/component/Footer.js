d8927a3c62a6fecaac4c6a8cb48ba9d2779f8498
Saqlain Malik <115017610+Saqlain-Malik313@users.noreply.github.com>
2025-03-13 00:47:15 +0530

    Update Footer.js

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Foot from './Foot';
+import { NavLink } from 'react-router-dom';
 
 const Footer = () => {
     return (
@@ -14,12 +15,15 @@ const Footer = () => {
                         and retain more users. Trusted by over 4,000 startups.</p>
                     <div className='center container-fluid' style={{paddingBottom:"30px"}}>
                         <div className="wrap container-fluid ">
-                            <Button variant="light" id='btn' size="lg" style={{ borderRadius: "5px",border:" .5 px black solid" }}>
-                                Learn more
+                            <Button variant="light" id='btn' size="lg" style={{ borderRadius: "5px",border:" .2 px black solid", color:"black" }}>
+                              <a href='https://www.geeksforgeeks.org/software-engineering-introduction-to-software-engineering/' style={{color:"black",}}>
+                             Learn more </a>
                             </Button>
+                            <NavLink to='/stared'>
                             <Button variant="danger" id='btn' size="lg" style={{ borderRadius: "5px",  }}>
                                 Get started
                             </Button>
+                            </NavLink>
                         </div>
                     </div>
                 </div>
