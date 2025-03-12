7813708c5699e0ac7f854f1fcf1cff2a8f242687
Saqlain Malik <115017610+Saqlain-Malik313@users.noreply.github.com>
2025-03-12 17:25:06 +0530

    Add files via upload

--- src/component/Main.js
+++ src/component/Main.js
@@ -8,15 +8,15 @@ import logo3 from "./logo/Fictional company logo (2).png"
 import logo4 from "./logo/Fictional company logo (3).png"
 import logo5 from "./logo/Fictional company logo (4).png"
 import logo6 from "./logo/Fictional company logo (5).png"
-
-import { Row , Col} from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import { Row , Col, } from 'react-bootstrap';
 
 
 
 const Main = () => {
     return (
         <>
-            <div className='container-fluid ' id='main'>
+            <div className='container-fluid ' id='main' >
                 <div className='center'>
                     <p class="round3"><small class="round2">New feature</small>Check out the team dashboard→</p>
                 </div>
@@ -29,21 +29,23 @@ const Main = () => {
                     <Button variant="light"  id='btn' size="lg">
                     <FaRegPlayCircle/> Demo
                     </Button>
+                    <NavLink to='/sign'>
                     <Button variant="danger" id='btn' size="lg">
                         sign up
                     </Button>
+                    </NavLink>
+                    
                 </div>
                 <div className=' center'>
                     <img src={dash} alt={dash} id='dash' width="700" height="300"/>
                 </div>
-                <div className='center'> Join 4,000+ companies already growing </div>
+                <div className='center container'> Join 4,000+ companies already growing </div>
                 <div className=''>
                     <Row className='cent'>
                         
-                        <Col><img src={logo1} alt={logo1} style={{width:"120px",height:"30px"}} /></Col>
+                        <Col><img src={logo1} alt={logo1} style={{width:"110px",height:"30px"}}/></Col>
                         <Col><img src={logo2} alt={logo2} style={{width:"120px",height:"30px"}}/></Col>
                         <Col><img src={logo3} alt={logo3} style={{width:"120px",height:"30px"}}/></Col>
-                        
                         <Col><img src={logo4} alt={logo4} style={{width:"120px",height:"30px"}}/></Col>
                         <Col><img src={logo5} alt={logo5} style={{width:"120px",height:"30px"}}/></Col>
                         <Col><img src={logo6} alt={logo6} style={{width:"120px",height:"30px"}}/></Col>
