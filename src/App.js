60632fc0186a50b5c001f5b187a676a431a7a753
Saqlain Malik <115017610+Saqlain-Malik313@users.noreply.github.com>
2025-03-13 00:42:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './Home';
 import Signn from './Signn';
+import Post from './Post';
+import Contact from './Contact';
+import Navba from './component/Navbar';
+import Started from './component/Started';
 
 const App=()=>{
   return<>
   <BrowserRouter>
+  <Navba/>
   <Routes>
-    <Route path='' element={<Home/>}/>
+    <Route path='/' element={<Home/>}/>
     <Route path='/sign' element={<Signn/>}/>
+    <Route path='/post' element={<Post/>}/>
+    <Route path='/contact' element={<Contact/>}/>
+    <Route path='/stared' element={<Started/>}/>
+
   </Routes>
   </BrowserRouter>
   </>
